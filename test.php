f5cf5290cce221cbf156667a6452007aac317751
Jessy <jessy.jeannin@gmail.com>
2018-12-06 16:01:03 +0100

    accueil

---
+++ test.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="test.css">
+    <title>Document</title>
+</head>
+<body>
+
+<h2>Zoe</h2>
+				<div class="grid">
+					<figure class="effect-zoe">
+						<img src="images/25.jpg" alt="img25"/>
+						<figcaption>
+							<h2>Creative <span>Zoe</span></h2>
+							<p class="icon-links">
+								<a href="#"><span class="icon-heart"></span></a>
+								<a href="#"><span class="icon-eye"></span></a>
+								<a href="#"><span class="icon-paper-clip"></span></a>
+							</p>
+							<p class="description">Zoe never had the patience of her sisters. She deliberately punched the bear in his face.</p>
+						</figcaption>			
+					</figure>
+</body>
+</html>
+
