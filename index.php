0fd42926a884983d5b49f0b51d674d2a1e28ca80
Reisenleiter bryan <reisenleiterbryan2@gmail.com>
2018-12-10 16:38:12 +0100

    fonctionne (normalement)

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/accueil.css">
+    <link rel="stylesheet" href="./css/Vinyle/accueil.css">
     <title>Accueil</title>
 </head>
 <body>
     <div class="container">
-        <div><a href=""><img class="art-menu" src="images/rocknpophotspace.png" alt=""></a></div>
-        <div><a href="./views/RecordPlayer/index.html"><img class="vinyle-menu" src="./images/Beatles-Pic.png" alt=""></a></div>
+        <div><a href=""><img class="art-menu" src="./images/Accueil/rocknpophotspace.png" alt=""></a></div>
+        <div><a href="./views/Vinyle/RecordPlayer/index.html"><img class="vinyle-menu" src="./images/Accueil/Beatles-Pic.png" alt=""></a></div>
     </div>
 </body>
 </html>
